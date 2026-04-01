f9e9287e6d94cf2e5beb3e2ca00d7c82fd105ad3
abishekhariharan076 <abishekhariharan76.@gmail.com>
2026-04-01 15:35:19 +0530

    Use hash routing for static deployment

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
@@ -14,14 +14,14 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     {hasGoogleClientId ? (
       <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
-        <BrowserRouter>
+        <HashRouter>
           <App />
-        </BrowserRouter>
+        </HashRouter>
       </GoogleOAuthProvider>
     ) : (
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     )}
   </React.StrictMode>
 );
